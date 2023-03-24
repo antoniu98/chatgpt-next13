526b9fbdf328d27ef227848f03c39beb45ff14a6
Antoniu Neacsu <aneacsu@adobe.com>
2023-03-24 15:15:42 +0200

    simple design center component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,50 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+import { SunIcon } from "@heroicons/react/24/outline";
 
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
+export default function Homepage() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
+    <div className="flex flex-col items-center justify-center h-screen px-2 text-white">
+      <h1 className="text-5xl font-bold mb-20">ChatGPT</h1>
+
+      <div className="flex text-center">
         <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+          {/* Sun */}
+          <div className="flex flex-col mb-5 items-center">
+            <SunIcon className=" h-20 w-20" />
+            <h2>Examples</h2>
+          </div>
+          <div className="space-y-2">
+            <p className="infoText">Explain smth to me SUN1</p>
+            <p className="infoText">Explain smth to me SUN2</p>
+            <p className="infoText">Explain smth to me SUN3</p>
+          </div>
+        </div>
+
+        <div>
+        {/* Sun */}
+        <div className="flex flex-col mb-5 items-center">
+          <SunIcon className=" h-20 w-20" />
+          <h2>Examples</h2>
+        </div>
+        <div className="space-y-2">
+          <p className="infoText">Explain smth to me SUN1</p>
+          <p className="infoText">Explain smth to me SUN2</p>
+          <p className="infoText">Explain smth to me SUN3</p>
         </div>
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
+        <div>
+          {/* Sun */}
+          <div className="flex flex-col mb-5 items-center">
+            <SunIcon className=" h-20 w-20" />
+            <h2>Examples</h2>
+          </div>
+          <div className="space-y-2">
+            <p className="infoText">Explain smth to me SUN1</p>
+            <p className="infoText">Explain smth to me SUN2</p>
+            <p className="infoText">Explain smth to me SUN3</p>
+          </div>
         </div>
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
