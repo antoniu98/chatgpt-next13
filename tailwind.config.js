01f31501dd9a38f49bd50ef175c0a2cbf0a29b28
Antoniu Neacsu <aneacsu@adobe.com>
2023-03-24 14:45:58 +0200

    configured tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  purge: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   content: [],
   theme: {
     extend: {},
