ba348874491284f2e9b3c95aa607a9e04430f864
Antoniu Neacsu <aneacsu@adobe.com>
2023-03-24 15:28:34 +0200

    styling sidebar

---
+++ src/components/NewChat.tsx
@@ -0,0 +1,10 @@
+import { PlusIcon } from "@heroicons/react/24/outline";
+
+export default function NewChat() {
+  return (
+    <div className="chatRow">
+        <PlusIcon className="h-8 w-8 text-white"/>
+        <p>New Chat</p>
+    </div>
+  )
+}
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,5 +1,17 @@
+import NewChat from "./NewChat";
+
 export default function SideBar() {
   return (
-    <div>SideBar</div>
+    <div className="p-2 flex flex-col h-screen bg-gray-900 overflow-y-scroll
+    max-w-xs md:min-w-[20rem]">
+        <div className="flex-1">
+            {/*  NewChat */}
+            <NewChat />
+
+            {/* Model selection */}
+
+            {/* Chat Lists */}
+        </div>
+    </div>
   )
 }
